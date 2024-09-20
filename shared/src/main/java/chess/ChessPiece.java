ef68bc1a57c6a01d92fe1b6a5cf4371cebf8ed87
Jaden <jadensk@byu.edu>
2024-09-20 10:06:05 -0600

    Bishop, King and Knight done

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -65,7 +65,7 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
-    private Collection<ChessMove> Bishop_Calc(ChessBoard board, ChessPosition position) {
+    private Collection<ChessMove> bishopCalc(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> Possibilities = new ArrayList<>();
         int row = position.getRow();
         int col = position.getColumn();
@@ -79,15 +79,13 @@ public class ChessPiece {
                     row += 1;
                     col += 1;
                     continue;
-                }
-                else {
+                } else {
                     ChessPiece myPiece = board.getPiece(position);
                     ChessPiece otherPiece = board.getPiece(aheadPosition);
                     if (myPiece.pieceColor != otherPiece.pieceColor) {
                         Possibilities.add(new ChessMove(new ChessPosition(start_row, start_col), new ChessPosition(row + 1, col + 1), null));
                         break;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
@@ -105,15 +103,13 @@ public class ChessPiece {
                     row += 1;
                     col -= 1;
                     continue;
-                }
-                else {
+                } else {
                     ChessPiece myPiece = board.getPiece(position);
                     ChessPiece otherPiece = board.getPiece(aheadPosition);
                     if (myPiece.pieceColor != otherPiece.pieceColor) {
                         Possibilities.add(new ChessMove(new ChessPosition(start_row, start_col), new ChessPosition(row + 1, col - 1), null));
                         break;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
@@ -131,15 +127,13 @@ public class ChessPiece {
                     row -= 1;
                     col -= 1;
                     continue;
-                }
-                else {
+                } else {
                     ChessPiece myPiece = board.getPiece(position);
                     ChessPiece otherPiece = board.getPiece(aheadPosition);
                     if (myPiece.pieceColor != otherPiece.pieceColor) {
                         Possibilities.add(new ChessMove(new ChessPosition(start_row, start_col), new ChessPosition(row - 1, col - 1), null));
                         break;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
@@ -157,15 +151,13 @@ public class ChessPiece {
                     row -= 1;
                     col += 1;
                     continue;
-                }
-                else {
-                    ChessPiece myPiece = board.getPiece(position);
-                    ChessPiece otherPiece = board.getPiece(aheadPosition);
-                    if (myPiece.pieceColor != otherPiece.pieceColor) {
+                } else {
+                    ChessPiece myBishop = board.getPiece(position);
+                    ChessPiece bishopNeighbor = board.getPiece(aheadPosition);
+                    if (myBishop.pieceColor != bishopNeighbor.pieceColor) {
                         Possibilities.add(new ChessMove(new ChessPosition(start_row, start_col), new ChessPosition(row - 1, col + 1), null));
                         break;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
@@ -176,10 +168,274 @@ public class ChessPiece {
         return Possibilities;
     }
 
+    private Collection<ChessMove> kingCalc(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> Possibilities = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+        Collection<ChessPosition> Positions = new ArrayList<>();
+        if (col - 1 > 0) {
+            Positions.add(new ChessPosition(row, col - 1));
+        }
+        if (col + 1 < 9) {
+            Positions.add(new ChessPosition(row, col + 1));
+        }
+        if (row - 1 > 0 && col - 1 > 0) {
+            Positions.add(new ChessPosition(row - 1, col - 1));
+        }
+        if (row - 1 > 0) {
+            Positions.add(new ChessPosition(row - 1, col));
+        }
+        if (row - 1 > 0 && col + 1 < 9) {
+            Positions.add(new ChessPosition(row - 1, col + 1));
+        }
+        if (row + 1 < 9 && col - 1 > 0) {
+            Positions.add(new ChessPosition(row + 1, col - 1));
+        }
+        if (row + 1 < 9) {
+            Positions.add(new ChessPosition(row + 1, col));
+        }
+        if (row + 1 < 9 && col + 1 < 9) {
+            Positions.add(new ChessPosition(row + 1, col + 1));
+        }
+        for (ChessPosition pos : Positions) {
+            if (board.getPiece(pos) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), pos, null));
+            } else {
+                ChessPiece myKing = board.getPiece(position);
+                ChessPiece kingNeighbor = board.getPiece(pos);
+                if (myKing.pieceColor != kingNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), pos, null));
+                }
+            }
+        }
+        return Possibilities;
+    }
+
+    private Collection<ChessMove> knightCalc(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> Possibilities = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+        Collection<ChessPosition> Positions = new ArrayList<>();
+        if (row + 2 < 9 && col + 1 < 9) {
+            ChessPosition aheadPosition1 = new ChessPosition(row + 2, col + 1);
+            if (board.getPiece(aheadPosition1) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition1, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition1);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition1, null));
+                }
+            }
+        }
+        if (row + 2 < 9 && col - 1 > 0) {
+            ChessPosition aheadPosition2 = new ChessPosition(row + 2, col - 1);
+            if (board.getPiece(aheadPosition2) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition2, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition2);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition2, null));
+                }
+            }
+        }
+        if (row + 1 < 9 && col + 2 < 9) {
+            ChessPosition aheadPosition3 = new ChessPosition(row + 1, col + 2);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        if (row + 1 < 9 && col - 2 > 0) {
+            ChessPosition aheadPosition3 = new ChessPosition(row + 1, col - 2);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        if (row - 1 > 0 && col + 2 < 9) {
+            ChessPosition aheadPosition3 = new ChessPosition(row - 1, col + 2);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        if (row - 1 > 0 && col - 2 > 0) {
+            ChessPosition aheadPosition3 = new ChessPosition(row - 1, col - 2);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        if (row - 2 > 0 && col + 1 < 9) {
+            ChessPosition aheadPosition3 = new ChessPosition(row - 2, col + 1);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        if (row - 2 > 0 && col - 1 > 0) {
+            ChessPosition aheadPosition3 = new ChessPosition(row - 2, col - 1);
+            if (board.getPiece(aheadPosition3) == null) {
+                Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+            } else {
+                ChessPiece myKnight = board.getPiece(position);
+                ChessPiece knightNeighbor = board.getPiece(aheadPosition3);
+                if (myKnight.pieceColor != knightNeighbor.pieceColor) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition3, null));
+                }
+            }
+        }
+        return Possibilities;
+    }
+    private Collection<ChessMove> pawnCalc(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> Possibilities = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+        if (board.getPiece(position).pieceColor == ChessGame.TeamColor.WHITE){
+            if (row == 2) {
+                ChessPosition aheadPosition = new ChessPosition(row + 1, col);
+                ChessPosition diagChecker1 = new ChessPosition(row + 1, col + 1);
+                ChessPosition diagChecker2 = new ChessPosition(row + 1, col - 1);
+                if (board.getPiece(aheadPosition) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition, null));
+                }
+                if (board.getPiece(diagChecker1) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker1);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker1, null));
+                    }
+                }
+                if (board.getPiece(diagChecker2) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker2);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker2, null));
+                    }
+                }
+                int row2 = position.getRow() + 1;
+                ChessPosition aheadPosition2 = new ChessPosition(row2 + 1, col);
+                if (board.getPiece(aheadPosition2) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row2, col), aheadPosition2, null));
+                }
+            }
+            else {
+                ChessPosition aheadPosition = new ChessPosition(row + 1, col);
+                ChessPosition diagChecker1 = new ChessPosition(row + 1, col + 1);
+                ChessPosition diagChecker2 = new ChessPosition(row + 1, col - 1);
+                if (board.getPiece(aheadPosition) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition, null));
+                }
+                if (board.getPiece(diagChecker1) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker1);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker1, null));
+                    }
+                }
+                if (board.getPiece(diagChecker2) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker2);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker2, null));
+                    }
+                }
+            }
+        }
+        else {
+            if (row == 7) {
+                ChessPosition aheadPosition = new ChessPosition(row - 1, col);
+                ChessPosition diagChecker1 = new ChessPosition(row - 1, col + 1);
+                ChessPosition diagChecker2 = new ChessPosition(row - 1, col - 1);
+                if (board.getPiece(aheadPosition) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition, null));
+                }
+                if (board.getPiece(diagChecker1) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker1);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker1, null));
+                    }
+                }
+                if (board.getPiece(diagChecker2) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker2);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker2, null));
+                    }
+                }
+                int row2 = position.getRow() - 1;
+                ChessPosition aheadPosition2 = new ChessPosition(row2 - 1, col);
+                if (board.getPiece(aheadPosition2) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row2, col), aheadPosition2, null));
+                }
+            }
+            else {
+                ChessPosition aheadPosition = new ChessPosition(row - 1, col);
+                ChessPosition diagChecker1 = new ChessPosition(row - 1, col + 1);
+                ChessPosition diagChecker2 = new ChessPosition(row - 1, col - 1);
+                if (board.getPiece(aheadPosition) == null) {
+                    Possibilities.add(new ChessMove(new ChessPosition(row, col), aheadPosition, null));
+                }
+                if (board.getPiece(diagChecker1) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker1);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker1, null));
+                    }
+                }
+                if (board.getPiece(diagChecker2) != null) {
+                    ChessPiece myPawn = board.getPiece(position);
+                    ChessPiece pawnNeighbor = board.getPiece(diagChecker2);
+                    if (myPawn.pieceColor != pawnNeighbor.pieceColor) {
+                        Possibilities.add(new ChessMove(new ChessPosition(row, col), diagChecker2, null));
+                    }
+                }
+            }
+        }
+        return Possibilities;
+    }
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         if (this.type == PieceType.BISHOP) {
-            return Bishop_Calc(board, myPosition);
+            return bishopCalc(board, myPosition);
         }
+        if (this.type == PieceType.KING) {
+            return kingCalc(board, myPosition);
+        }
+        if (this.type == PieceType.KNIGHT) {
+            return knightCalc(board, myPosition);
+        }
+        if (this.type == PieceType.PAWN) {
+            return pawnCalc(board, myPosition);
+        }
+
         return null;
     }
 }
