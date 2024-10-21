cc41d3bc25fc2c410cc80b3adfb3f2320c97119e
Jaden <jadensk@byu.edu>
2024-10-21 15:50:05 -0600

    server clear blueprinted

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,28 @@
 package service;
-
+import dataaccess.*;
+class ClearResponse {
+    public ClearResponse(String response) {
+        this.response = response;
+    }
+    public String getResponse() {
+        return response;
+    }
+    public void setResponse(String response) {
+        this.response = response;
+    }
+    private String response;
+}
 public class ClearService {
+    public ClearResponse clearApplication() {
+        GameDAO game = new GameDAO();
+        UserDAO user = new UserDAO();
+        AuthDAO auth = new AuthDAO();
+        try {
+            
+        }
+        catch (DataAccessException e) {
+            return new ClearResponse(e.getMessage());
+        }
+        return new ClearResponse();
+    }
 }
--- server/src/test/java/service/ServiceUnitTests.java
+++ server/src/test/java/service/ServiceUnitTests.java
@@ -1,4 +1,12 @@
 package service;
-
+import chess.ChessGame;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import service.*;
+import model.*;
+import java.util.ArrayList;
 public class ServiceUnitTests {
+
 }
