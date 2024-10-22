06cff793bff25d1f4e73a950b37d6805182b6d23
Jaden <jadensk@byu.edu>
2024-10-22 12:56:54 -0600

    Clear Service

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -7,22 +7,21 @@ class ClearResponse {
     public String getResponse() {
         return response;
     }
-    public void setResponse(String response) {
-        this.response = response;
-    }
     private String response;
 }
 public class ClearService {
-    public ClearResponse clearApplication() {
+    public ClearResponse clearEverything() {
         GameDAO game = new GameDAO();
         UserDAO user = new UserDAO();
         AuthDAO auth = new AuthDAO();
         try {
-
+            game.clear();
+            user.clear();
+            auth.clear();
         }
-        catch (DataAccessException e) {
-            return new ClearResponse(e.getMessage());
+        catch (DataAccessException exception) {
+            return new ClearResponse(exception.getMessage());
         }
-        return new ClearResponse();
+        return new ClearResponse("");
     }
 }
