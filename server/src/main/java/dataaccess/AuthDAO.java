fd9455492c1fe077d7f7220097cadc6228957f39
Jaden <jadensk@byu.edu>
2024-11-01 16:08:27 -0600

    DataBase Created

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,10 +1,13 @@
 package dataaccess;
 import java.util.*;
-
 import model.AuthData;
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class AuthDAO {
     private static HashMap<String, AuthData> authMap = new HashMap<>();
+    private DatabaseManager dbMan = new DatabaseManager();
+
     public void createToken(AuthData auth) throws DataAccessException{
         authMap.put(auth.getAuth(), auth);
     }
