d10a6b2fdeb76e7b0cf613822455966eb087df39
Jaden <jadensk@byu.edu>
2024-11-14 14:18:39 -0700

    Register Ready

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,38 @@
+package ui;
+import requestandresponse.*;
+import com.google.gson.*;
+import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class ServerFacade {
+    private final String serverUrl = "http://localhost:8080";
+
+    public RegResponse register(RegRequest req) throws IOException {
+        URL url = new URL(serverUrl + "/user");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(5000);
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.connect();
+        try (var outputStream = conn.getOutputStream()) {
+            outputStream.write(new Gson().toJson(req).getBytes());
+        }
+
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            RegResponse resp = null;
+            try (InputStream stream = conn.getErrorStream()) {
+                InputStreamReader streamReader = new InputStreamReader(stream);
+                resp = new Gson().fromJson(streamReader, RegResponse.class);
+            }
+            return resp;
+        } else {
+            RegResponse resp = null;
+            try (InputStream stream = conn.getInputStream()) {
+                InputStreamReader streamReader = new InputStreamReader(stream);
+                resp = new Gson().fromJson(streamReader, RegResponse.class);
+            }
+            return resp;
+        }
+    }
+}
