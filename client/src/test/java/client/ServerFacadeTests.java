eec36c317ebdf6683f813c25a819bf7e706c9664
Jaden <jadensk@byu.edu>
2024-11-14 18:30:24 -0700

    CreateGame and LogOut

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,11 +1,11 @@
 package client;
 
 import org.junit.jupiter.api.*;
-import requestandresponse.RegRequest;
-import requestandresponse.RegResponse;
+import requestandresponse.*;
 import server.Server;
 import service.ClearService;
 import ui.ServerFacade;
+import ui.TokenPlaceholder;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -61,6 +61,72 @@ public class ServerFacadeTests {
         try{
             RegResponse response = facade.register(request2);
             assertEquals("Error! Name already being used!", response.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    void LoginSuccess(){
+        RegRequest request = new RegRequest("Jadenizer", "Earthbounder4", "kunzlerj9@gmail.com");
+        try {
+            facade.register(request);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        LoginRequest loginRequest = new LoginRequest("Jadenizer", "Earthbounder4");
+        try{
+            LoginResponse response = facade.login(loginRequest);
+            assertNull(response.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void LoginFail(){
+        RegRequest request = new RegRequest("Jadenizer", "Earthbounder4", "kunzlerj9@gmail.com");
+        try {
+            facade.register(request);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        LoginRequest loginRequest = new LoginRequest("NotJaden", "ImSneakingInNow");
+        try{
+            LoginResponse response = facade.login(loginRequest);
+            assertEquals("Error! Wrong password", response.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    void Logout(){
+        RegRequest request = new RegRequest("Jadenizer", "Earthbounder4", "kunzlerj9@gmail.com");
+        try {
+            facade.register(request);
+            LoginRequest loginRequest = new LoginRequest("Jadenizer", "Earthbounder4");
+            LoginResponse response = facade.login(loginRequest);
+            assertNull(response.getMessage());
+            TokenPlaceholder.token = response.getAuth();
+            LogoutResponse response2 = facade.logout();
+            assertNull(response2.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    void CreateGame(){
+        RegRequest request1 = new RegRequest("Jadenizer", "Earthbounder4", "kunzlerj9@gmail.com");
+        LoginRequest request2 = new LoginRequest("Jadenizer", "Earthbounder4");
+        CreateGameRequest request3 = new CreateGameRequest("Jaden's Game");
+        try{
+            facade.register(request1);
+            LoginResponse response = facade.login(request2);
+            TokenPlaceholder.token = response.getAuth();
+            CreateGameResponse response2 = facade.createGame(request3);
+            assertNull(response2.getMessage());
         }
         catch(IOException e){
             System.out.println(e.getMessage());
