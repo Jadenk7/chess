01c388b56a3067811f585626390b9a5dda812cc6
Jaden <jadensk@byu.edu>
2024-11-21 14:07:42 -0700

    Client Divided into Handler Methods

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -10,139 +10,30 @@ import model.*;
 public class Main {
 
     private static void loggedInCommands(ServerFacade server) throws IOException {
-        System.out.print("\n");
-        System.out.println("\"list\" - to list all games");
-        System.out.println("\"create\" - to create and name a new game");
-        System.out.println("\"join\" - to join a game and pick your color. ID required");
-        System.out.println("\"observe\" - to observe a game. ID required");
-        System.out.println("\"help\" - to list possible commands");
-        System.out.println("\"logout\" - to log out");
-        System.out.println("\"quit\" - to quit your game");
+        System.out.println();
+        displayLoggedInMenu();
         Scanner scanner = new Scanner(System.in);
         String command = scanner.next();
         scanner.nextLine();
-        switch(command){
+        switch (command) {
             case "list":
-                ListGamesResponse resp = server.listgames();
-                if(resp.getMessage() != null){
-                    System.out.println(resp.getMessage());
-                    loggedInCommands(server);
-                    break;
-                }
-                else{
-                    System.out.println("Current Games:");
-                    for(GameData game: resp.getGames()){
-                        System.out.println();
-                        System.out.println(game.getGameName());
-                        System.out.println("Game ID: " + game.getID());
-                        System.out.println("White Player: " + game.getWhiteUsername());
-                        System.out.println(" Black Player: " + game.getBlackUsername());
-                    }
-                    loggedInCommands(server);
-                    break;
-                }
+                handleListGames(server);
+                break;
             case "create":
-                System.out.println("Name of your new Game:");
-                String name = scanner.nextLine();
-                CreateGameRequest req = new CreateGameRequest(name);
-                CreateGameResponse response = server.createGame(req);
-                if(response.getMessage() != null) {
-                    System.out.println(response.getMessage());
-                    loggedInCommands(server);
-                    break;
-                }
-                else{
-                    System.out.println("<Successful Game Creation>");
-                    System.out.println("Game ID: " + response.getID());
-                    System.out.println();
-                    loggedInCommands(server);
-                    break;
-                }
+                handleCreateGame(server);
+                break;
             case "join":
-                ChessGame.TeamColor color = null;
-                int validator = 0;
-                while(validator == 0) {
-                    System.out.println("Select Team Color: \"black\" or \"white\"");
-                    String selected = scanner.nextLine();
-                    if (selected.equals("white")) {
-                        color = ChessGame.TeamColor.WHITE;
-                        validator += 1;
-                    }
-                    if (selected.equals("black")) {
-                        color = ChessGame.TeamColor.BLACK;
-                        validator += 1;
-                    }
-                }
-                int id;
-                try {
-                    System.out.println("Game ID: ");
-                    id=scanner.nextInt();
-                }
-                catch (java.util.InputMismatchException e) {
-                    System.out.println("Input not accepted");
-                    scanner.nextLine();
-                    loggedInCommands(server);
-                    break;
-                }
-                JoinGameRequest request = new JoinGameRequest(color, id);
-                JoinGameResponse myResponse = server.joinGame(request);
-                if (myResponse.getMessage() != null) {
-                    System.out.println(myResponse.getMessage());
-                    loggedInCommands(server);
-                    break;
-                }
-                else {
-                    System.out.println("<Successfully Joined Game>");
-                    ChessBoard board = new ChessBoard();
-                    board.resetBoard();
-                    PrintBoard.drawForPlayer1(board);
-                    System.out.print("\n");
-                    PrintBoard.drawForPlayer2(board);
-                    break;
-                }
+                handleJoinGame(server);
+                break;
             case "observe":
-                int gameID;
-                try {
-                    System.out.println("Game ID: ");
-                    gameID=scanner.nextInt();
-                }
-                catch(java.util.InputMismatchException e){
-                    System.out.println("Input not accepted");
-                    scanner.nextLine();
-                    loggedInCommands(server);
-                    break;
-                }
-                JoinGameRequest req1 = new JoinGameRequest(null, gameID);
-                JoinGameResponse resp1 = server.joinGame(req1);
-                if(resp1.getMessage() != null) {
-                    System.out.println(resp1.getMessage());
-                    loggedInCommands(server);
-                    break;
-                }
-                else{
-                    System.out.println("<Successfully Joined as Observer>");
-                    ChessBoard board = new ChessBoard();
-                    board.resetBoard();
-                    PrintBoard.drawForPlayer1(board);
-                    System.out.print("\n");
-                    PrintBoard.drawForPlayer2(board);
-                    break;
-                }
+                handleObserveGame(server);
+                break;
             case "help":
                 loggedInCommands(server);
                 break;
             case "logout":
-                LogoutResponse resp2 = server.logout();
-                if(resp2.getMessage() != null){
-                    System.out.println(resp2.getMessage());
-                    break;
-                }
-                else{
-                    System.out.println("<Successful Logout>");
-                    System.out.print("\n");
-                    listCommands();
-                    break;
-                }
+                handleLogout(server);
+                break;
             case "quit":
                 System.out.println("<Exited the Program>");
                 System.exit(0);
@@ -151,6 +42,127 @@ public class Main {
                 System.out.println("<Command not accepted>");
                 loggedInCommands(server);
                 break;
+        }
+    }
+    private static void displayLoggedInMenu() {
+        System.out.println("\"list\" - to list all games");
+        System.out.println("\"create\" - to create and name a new game");
+        System.out.println("\"join\" - to join a game and pick your color. ID required");
+        System.out.println("\"observe\" - to observe a game. ID required");
+        System.out.println("\"help\" - to list possible commands");
+        System.out.println("\"logout\" - to log out");
+        System.out.println("\"quit\" - to quit your game");
+    }
+
+    private static void handleListGames(ServerFacade server) throws IOException {
+        ListGamesResponse resp = server.listgames();
+        if (resp.getMessage() != null) {
+            System.out.println(resp.getMessage());
+        } else {
+            System.out.println("Current Games:");
+            for (GameData game : resp.getGames()) {
+                System.out.println();
+                System.out.println(game.getGameName());
+                System.out.println("Game ID: " + game.getID());
+                System.out.println("White Player: " + game.getWhiteUsername());
+                System.out.println("Black Player: " + game.getBlackUsername());
+            }
+        }
+        loggedInCommands(server);
+    }
+    private static void handleCreateGame(ServerFacade server) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name of your new Game:");
+        String name = scanner.nextLine();
+        CreateGameRequest req = new CreateGameRequest(name);
+        CreateGameResponse response = server.createGame(req);
+        if (response.getMessage() != null) {
+            System.out.println(response.getMessage());
+        } else {
+            System.out.println("<Successful Game Creation>");
+            System.out.println("Game ID: " + response.getID());
+            System.out.println();
+        }
+        loggedInCommands(server);
+    }
+
+    private static void handleJoinGame(ServerFacade server) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ChessGame.TeamColor color = selectTeamColor(scanner);
+        int id;
+        try {
+            System.out.println("Game ID: ");
+            id = scanner.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Input not accepted");
+            scanner.nextLine();
+            loggedInCommands(server);
+            return;
+        }
+        JoinGameRequest request = new JoinGameRequest(color, id);
+        JoinGameResponse response = server.joinGame(request);
+        if (response.getMessage() != null) {
+            System.out.println(response.getMessage());
+            loggedInCommands(server);
+        } else {
+            System.out.println("<Successfully Joined Game>");
+            ChessBoard board = new ChessBoard();
+            board.resetBoard();
+            PrintBoard.drawForPlayer1(board);
+            System.out.println();
+            PrintBoard.drawForPlayer2(board);
+        }
+    }
+    private static ChessGame.TeamColor selectTeamColor(Scanner scanner) {
+        ChessGame.TeamColor color = null;
+        int validator = 0;
+        while (validator == 0) {
+            System.out.println("Select Team Color: \"black\" or \"white\"");
+            String selected = scanner.nextLine();
+            if (selected.equals("white")) {
+                color = ChessGame.TeamColor.WHITE;
+                validator++;
+            } else if (selected.equals("black")) {
+                color = ChessGame.TeamColor.BLACK;
+                validator++;
+            }
+        }
+        return color;
+    }
+    private static void handleObserveGame(ServerFacade server) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int gameID;
+        try {
+            System.out.println("Game ID: ");
+            gameID = scanner.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Input not accepted");
+            scanner.nextLine();
+            loggedInCommands(server);
+            return;
+        }
+        JoinGameRequest request = new JoinGameRequest(null, gameID);
+        JoinGameResponse response = server.joinGame(request);
+        if (response.getMessage() != null) {
+            System.out.println(response.getMessage());
+            loggedInCommands(server);
+        } else {
+            System.out.println("<Successfully Joined as Observer>");
+            ChessBoard board = new ChessBoard();
+            board.resetBoard();
+            PrintBoard.drawForPlayer1(board);
+            System.out.println();
+            PrintBoard.drawForPlayer2(board);
+        }
+    }
+    private static void handleLogout(ServerFacade server) throws IOException {
+        LogoutResponse response = server.logout();
+        if (response.getMessage() != null) {
+            System.out.println(response.getMessage());
+        } else {
+            System.out.println("<Successful Logout>");
+            System.out.println();
+            Main.listCommands();
         }
     }
     public static void listCommands() {
